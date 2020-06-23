fe12fbbe01a9e389e8eed6f333ac404816eab0db
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 03:27:11 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,8 +1,8 @@
-## code your solution here. 
+## code your solution here.
 # Cat instantiates a new cat
 # Cat receives attr_accessor
 # Cat has a name
 #Cat is able to meow
 
-class Cat 
+class Cat
 end 
